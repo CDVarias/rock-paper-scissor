d6e3a40789e9b9d71e4993ba50d07fcaa9cff388
CD Varias <variascieldean@gmail.com>
2023-07-05 10:25:58 +0800

    Foundations (Rock Paper, and Scissors) Done!

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const options = ["rock","paper","scissors"]; //computer choices
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random()* options.length)]
-    console.log(choice);
     return choice;
     // para makapili randomly sa tatlong nasa choices ang computer
 }
@@ -41,6 +40,49 @@ function playRound(playerSelection,computerSelection){
     //dito naman sa first if statement ay, kapag ang result ay kapag panalo ang computer sa function ng checkWinner if same ang choice. then yung irereturn naman nya ay it is a tie with console.log
 }
 
-const playerSelection = "rock"; //player input
-const computerSelection = getComputerChoice(); //computer input
-console.log(playRound(playerSelection, computerSelection)); //pinalabas dito yung nireturn galing sa playRound function
+
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice  = prompt("Rock Paper or Scissors");
+        if(validatedInput == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        } 
+    }
+}
+
+
+function game (){
+// console.log(playRound(playerSelection, computerSelection)); //pinalabas dito yung nireturn galing sa playRound function
+    let playerScore = 0;
+    let computerScore = 0;
+    for (i=0; i<5; i++){
+        const playerSelection = getPlayerChoice(); //player input
+        const computerSelection = getComputerChoice(); //computer input
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("_____________")
+        if (checkWinner(playerSelection,computerSelection) == "Player"){
+            playerScore++
+        }
+        else if (checkWinner(playerSelection,computerSelection) == "Computer"){
+            computerScore++;
+        }
+    }
+    console.log("Game Over!")
+    if (playerScore > computerScore){
+        console.log("Player is the Winner!")
+    }
+    else if (computerScore > playerScore){
+        console.log("Computer is the Winner!")
+    }
+    else{
+        console.log("We have a Tie!");
+    }
+}
+
+game();
